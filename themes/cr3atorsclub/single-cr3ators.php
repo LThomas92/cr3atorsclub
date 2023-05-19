f5015845d3cafef0ccb298741196faaf77c38100
Lawrence <lthomas@administrators-iMac.local>
2023-05-19 16:21:18 -0400

    updates to podcast page

--- themes/cr3atorsclub/single-cr3ators.php
+++ themes/cr3atorsclub/single-cr3ators.php
@@ -13,6 +13,35 @@
                 </figure>
             </div> <!-- TOP SECTION -->
 
+            <div class="c-single-cr3ators__social">
+                <?php $followOn = get_field('follow_on'); ?>
+                <div class="c-single-cr3ators__social-section">
+
+                <p class="c-single-cr3ators__follow-on"><?php echo $followOn; ?>:</p>
+                <?php $socialMediaIcons = get_field('social_media_items'); ?>
+                <?php if( have_rows('social_media_items') ): ?>
+                    <ul class="c-single-cr3ators__sm-items">
+                      <?php while( have_rows('social_media_items') ) : the_row();
+
+                        $smIcon = get_sub_field('social_media_icon'); 
+                        $smLink = get_sub_field('social_media_link');
+                        ?>
+                        <li class="c-single-cr3ators__sm-item">
+                            <a target="_blank" href="<?php echo $smLink; ?>">
+                            <img src="<?php echo $smIcon['url']; ?>" alt="<?php echo $smIcon['alt']; ?>">
+                            </a>
+                        </li>
+
+                    <?php
+                    endwhile; ?>
+                    </ul>
+
+                <?php else :
+                endif; ?>
+                </div>
+
+            </div>
+
             <div class="c-single-cr3ators__main-content">
                 <?php the_content(); ?>
             </div> <!-- MAIN CONTENT -->
