45664ea2337380e53c0921fee5e4e751afc278bc
Lawrence <lthomas@administrators-iMac.local>
2023-05-26 15:24:57 -0400

    updates to pages

--- themes/cr3atorsclub/page-shop.php
+++ themes/cr3atorsclub/page-shop.php
@@ -2,10 +2,37 @@
 
 <div class="content">
     <div class="c-page-shop">
-        <div class="c-page-shop__header">
-        </div>
         <div class="container-margins">
-
+            <div class="c-page-shop__featured-products-section">
+            <?php $featured_products = get_field('featured_products');
+            if( $featured_products ): ?>
+                <ul class="c-page-shop__featured-products">
+                    <?php $i = 0; ?>
+                <?php foreach( $featured_products as $featured_product ): 
+                    $permalink = get_permalink( $featured_product->ID );
+                    $title = get_the_title( $featured_product->ID );
+                    $image = get_the_post_thumbnail_url($featured_product->ID);
+                    ?>
+                    <?php if($i == 0) {?>
+                    <li class="c-page-shop__big-featured-product" key="<?php echo $i;?>">
+                        <figure>
+                            <img src="<?php echo $image; ?>" alt="">
+                        </figure>
+                        <h4><?php echo $title; ?></h4>
+                    </li>
+                    <?php } else { ?>
+                        <li class="c-page-shop__featured-product" key="<?php echo $i;?>">
+                        <figure>
+                            <img src="<?php echo $image; ?>" alt="">
+                        </figure>
+                        <h4><?php echo $title; ?></h4>
+                    </li>
+                    <?php } ?>
+                    <?php $i++; ?>
+                <?php endforeach; ?>
+                </ul>
+            <?php endif; ?>
+            </div>
         </div>
     </div>
 </div>
--- themes/cr3atorsclub/single-podcasts.php
+++ themes/cr3atorsclub/single-podcasts.php
@@ -1,13 +1,56 @@
 <?php get_header(); 
     $podcastHeader = get_field('podcast_thumbnail');
-    $podcastAudio = get_field('podcast_player');
+    $podcastAudio = get_field('podcast_audio');
+    $podcastGuest = get_field('podcast_guest');
+    $podcastAbout = get_field('podcast_about');
+    $podcastTranscript = get_field('podcast_transcript');
+    $podcastCode = get_field('podcast_code');
 ?>
 
 <div class="content">
     <div class="c-podcast">
+    <div class="container-margins">
         <div style="background-image: url('<?php echo $podcastHeader['url']; ?>')" class="c-podcast__player">
-            <div class="c-podcast__audio"><?php echo $podcastAudio; ?></div>
+            <div class="c-podcast__audio">
+                <audio class="audio-player" controls>
+                <source src="<?php echo $podcastAudio; ?>" type="audio/mpeg">
+                Your browser does not support the audio tag.
+                </audio> 
+            </div>
         </div>
+
+        <div class="c-podcast__author-title">
+                <p class="c-podcast__guest"><?php echo $podcastGuest; ?></p>
+                <h1 class="c-podcast__title">"<?php the_title(); ?>"</h1>
+            </div>
+        <ul class="c-podcast__about">
+            <li>About This Podcast</li>
+            <li>Transcript</li>
+        </ul>
+        <div class="c-podcast__about-podcast">
+            <?php echo $podcastAbout; ?>
+        </div>
+        <div class="c-podcast__transcript">
+            <?php echo $podcastTranscript; ?>
+        </div>
+        <div class="c-podcast__podcast-meta">
+            <div class="c-podcast__listening-opts"><span>Also listen on: 
+                <a target="_blank" href="">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/apple-podcast.png"/>
+                </a>
+                <a target="_blank" href="">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/spotify.png"/>
+                </a>
+        </span>
+        </div>
+        <a target="_blank" href="<?php echo $podcastVideoLink; ?>" class="c-podcast__podcast-video">Watch here: <img src="<?php echo get_template_directory_uri(); ?>/img/logo-yt.png"/></a>
+        </div>
+
+        <div class="c-podcast__share-podcast-info">
+            <p>Share Podcast</p>
+        </div>
+
+    </div>
     </div> <!-- c-podcast -->
 </div> <!-- content -->
 
